18e9179fb6696591db5454e993cbb1c42687088b
HasithDeAlwis <hasithde24@gmail.com>
2023-09-17 12:57:32 -0400

    Push and pop methods made  for Stack class

--- Node.java
+++ Node.java
@@ -1,8 +1,12 @@
 public class Node {
+    //Either a number or an operator (ie. *, /, + etc.)
     String cargo;
+    //Points to the next node that is linked to the current node
     Node next;
+    //Determines whether or not a piece of cargo is an operation or not
     Boolean isOperation;
 
+    //Constructors for the Node class
     public Node()
     {
         this.next = null;
@@ -10,9 +14,16 @@ public class Node {
         this.isOperation = null;
     }
 
-    public Node(Node next, String cargo)
+    public Node(String cargo, Node next)
     {
         this.next = next;
+        this.cargo = cargo;
+        this.isOperation = null;
+    }
+    
+    public Node(String cargo)
+    {
+        this.next = null;
         this.cargo = cargo;
         this.isOperation = null;
     }
--- Stack.java
+++ Stack.java
@@ -1,3 +1,39 @@
 public class Stack {
+    //Top of the stack
+    Node head;
+    
 
+    //Constructors for Stack
+    public Stack()
+    {
+        this.head = null;
+    }
+
+    public Stack(Node head)
+    {
+        this.head = head;
+    }
+
+    /**
+     * This method adds a new node at the top off the stack with a specified cargo
+     * @param cargo The cargo for the next element
+     */
+    public void push(String cargo)
+    {
+        //Make this.head.next point to the original top of the stack and change its cargo
+        this.head = new Node(cargo, this.head);
+    }
+    /**
+     * Remove the top most element of a stack
+     * @return The node of the top most element
+     */
+    public Node pop()
+    {
+        //Dummy node
+        Node holder = this.head;
+        //make this.head equal to the next Node, removing that node from the stack
+        this.head = this.head.next;
+        //Return the dummy node
+        return holder;
+    }
 }
